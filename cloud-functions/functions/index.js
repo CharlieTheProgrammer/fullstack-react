28c6c1f102e2fefaa880ee8e9a21c1f6c13cb1b6
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2019-08-22 15:11:57 -0700

    Added routers to app.

--- cloud-functions/functions/index.js
+++ cloud-functions/functions/index.js
@@ -3,9 +3,17 @@ const express = require('express')
 const app = express();
 const messages = require('./features/networkResponses/networkResponses')
 
+const commentsRouter = require('./routers/comments')
+const likesRouter = require('./routers/likes')
+const notificationsRouter = require('./routers/notifications')
 const postsRouter = require('./routers/posts')
+const usersRouter = require('./routers/users')
 
+app.use('/comments', commentsRouter)
+app.use('/likes', likesRouter)
+app.use('/notifications', notificationsRouter)
 app.use('/posts', postsRouter)
+app.use('/users', usersRouter)
 
 
 app.all('*', (req, res) => {
