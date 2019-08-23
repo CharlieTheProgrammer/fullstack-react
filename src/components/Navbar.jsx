af7d9f5b19a66bb031f246b00455e34b75fc0671
Carlos Ontiveros <charlietheprogrammer@outlook.com>
2019-08-22 20:22:47 -0700

    Reformatted code.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import Link from 'react-router-dom/Link'
+import { Link } from "react-router-dom";
 // MUI
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 
-const ToolbarStyles={
-	margin: 'auto'
-}
+const ToolbarStyles = {
+  margin: "auto"
+};
 
 export default class Navbar extends Component {
   render() {
@@ -15,9 +15,15 @@ export default class Navbar extends Component {
       <div>
         <AppBar position="fixed">
           <Toolbar style={ToolbarStyles}>
-            <Button color="inherit" component={Link} to="/">Home</Button>
-            <Button color="inherit" component={Link} to="/login">Login</Button>
-            <Button color="inherit" component={Link} to="/signup">Signup</Button>
+            <Button color="inherit" component={Link} to="/">
+              Home
+            </Button>
+            <Button color="inherit" component={Link} to="/login">
+              Login
+            </Button>
+            <Button color="inherit" component={Link} to="/signup">
+              Signup
+            </Button>
           </Toolbar>
         </AppBar>
       </div>
